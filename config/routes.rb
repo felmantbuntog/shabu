913f1caa62a11c002f1cf16c68fe4b0118453da2
FELMAN <felmantbuntog@gmail.com>
2015-07-16 16:56:51 +0800

    added product crud operation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'products/index'
+
   get 'home/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -7,6 +9,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root 'home#index'
+  
+  resources :products
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
