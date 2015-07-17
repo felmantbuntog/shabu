987984eea159c1c7f10603730bd5a1b58e38f822
jameshwartlopez <jameshwartlopez@gmail.com>
2015-07-17 17:01:44 +0800

    save use and redirect to home page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,16 @@
 class UsersController < ApplicationController
 	
 	def create
-		
+		@user = User.new(user_params)	
+ 		@user.save
+ 		redirect_to root_url
+	end
+	def edit
+  end
+
+	private
+
+	def user_params
+		 params.require(:user).permit(:email, :password, :firstname, :lastname, :address, :phonenumber)
 	end
 end
