132c1c7698ecde142d99f8ae3f90d517b19a9d35
jameshwartlopez <jameshwartlopez@gmail.com>
2015-07-22 17:36:42 +0800

    registration page

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,9 +1,7 @@
 $(document).ready(function(){
 
 	$("#frm_registration").submit(function(event){
-		var submit = false;
 		
-		termsIsChecked 		= $("#user_terms_of_service").is(":checked");
 		uEmail 				= $("#user_email");
 		uPassword 			= $("#user_password");
 		uConfirmPassword 	= $("#user_password_confirmation");
@@ -20,53 +18,46 @@ $(document).ready(function(){
 			if(!IsEmail(uEmail.val())){
 				$("label[for='user_email']").addClass("color-red").html("Email Address is invalid");
 				return false;
-			}else if(IsEmail(uEmail.val())){
-				$("label[for='user_email']").removeClass("color-red");
-				return true;
+			}else{
+				$("label[for='user_email']").removeClass("color-red").html("Email Address");
 			}
 			
 		}
 
 		if(uPassword.val().length <= 0){
-		   //uPassword.after("<p>Password can't be blank</p>");
 		   $("label[for='user_password']").addClass("color-red").html("Password is empty");
 		   return false;
 		}else if(uPassword.length >0){
 			 $("label[for='user_password']").removeClass("color-red");
-			return true;
 		}
 
-		// if(uConfirmPassword.length < 0){
-		// 	uConfirmPassword.after("<p>Confirm password can't be blank</p>");
-		// 	submit = false;
-		// }else{
-		// 	submit = false;
-		// }
+		if(uConfirmPassword.val().length <= 0){
+			uConfirmPassword.after("<p>Confirm password can't be blank</p>");
+			return false;
+		}else if(uConfirmPassword.val().length > 0){
+			 console.log(uPassword.val()+" ==== "+ uConfirmPassword.val());
+			 if(uPassword.val() !== uConfirmPassword.val()){
+			 	$("label[for='user_password']").addClass("color-red");
+			 	$("label[for='user_password_confirmation']").addClass("color-red").html("Password Don't Match");
+			 	return false;
+			 }
+		}
 
-		// if(uFirstName.length < 0){
-		// 	uFirstName.after("<p>First Name can't be blank</p>");
-		// 	submit = false;
-		// }else {
-		// 	submit = false;
-		// }
+		if(uFirstName.val().length <= 0){
+			$("label[for='user_firstname']").addClass("color-red").html("First Name is empty");
+			return false;
+		}
 
-		// if(uLastName.length < 0){
-		// 	uLastName.after("<p>Last Name can't be blank</p>");
-		// 	submit = false;
-		// }else {
-		// 	submit = false;
-		// }
+		if(uLastName.val().length <= 0){
+			$("label[for='user_lastname']").addClass("color-red").html("First Name is empty");
+			return false;
+		}
 
-		// if(uContactNumber.length < 0){
-		// 	uContactNumber.after("<p>Contact Number can't be blank</p>");
-		// 	submit = false;
-		// }else {
-		// 	submit = false;
-		// }
+		if(uContactNumber.val().length < 0){
+			$("label[for='user_phonenumber']").addClass("color-red").html("Contact number is empty");
+			return false;
+		}
 
-		
-		
-		
 	});
 
 	function IsEmail(email) {
