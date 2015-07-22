16c4b8496ae7e33de927426c2683b94e870a32dc
FELMAN <felmantbuntog@gmail.com>
2015-07-22 16:16:46 +0800

    added associations to User and Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
-
+  belongs_to :user
   mount_base64_uploader :image, ImageUploader
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ActiveRecord::Base
 	# validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 
 	# validates_presence_of :email,:password,:firstname,:lastname,:address,:phonenumber
+	has_many :products
 end
