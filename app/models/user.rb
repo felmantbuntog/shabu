8038317ea6e78be639524a39158e14b238e6ac10
jameshwartlopez <jameshwartlopez@gmail.com>
2015-07-23 18:30:02 +0800

    validate email in rails server if it is already taken

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
-	validates :terms_of_service, acceptance:{ :accept => '0'}
+	 validates :terms_of_service, acceptance:{ :accept => '0'}
 	 validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-
+ 	 validates :email, :presence => true, :uniqueness => true
 	 validates_presence_of :email,:password,:firstname,:lastname,:address,:phonenumber
-	has_many :products
+	 has_many :products
 
 	def self.search(email)
 		if email
