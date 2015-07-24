3111856f899dc7339b2a1ccf90dc6d334e6e54c7
FELMAN <felmantbuntog@gmail.com>
2015-07-24 11:00:16 +0800

    make it better!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,35 +2,36 @@ class UsersController < ApplicationController
     def index
 
     end
-	
-	def new 
-		@user = User.new
-	end
 
-	def create
-		@user = User.new(user_params)
- 		if @user.save
- 			redirect_to root_url
- 		else
- 			render 'new'
- 		end
-
-
-	end
-
-	 
-
-	def edit
+  	def new
+  		@user = User.new
   	end
-  	def emailcheck
-  		@user = User.search(params[:email])
-  		respond_to do |format|
-  			format.json {render :json => {email_exists: @user.present?}}
-  		end 
-  	end
-	private
 
-	def user_params
-		 params.require(:user).permit(:email, :password, :firstname, :lastname, :address, :phonenumber)
-	end
+  	def create
+  		@user = User.new(user_params)
+        respond_to do |format|
+       		if @user.save
+            format.html { redirect_to root_path, success: 'Registration successfull.' }
+            format.json { render :show, status: :created, location: @user }
+       		else
+            format.html { render :new }
+            format.json { render json: @user.errors, status: :unprocessable_entity }
+       		end
+        end
+  	end
+
+  	def edit
+    end
+
+    def emailcheck
+    	@user = User.search(params[:email])
+      	respond_to do |format|
+      		format.json {render :json => {email_exists: @user.present?}}
+      	end
+    end
+
+    private
+  	def user_params
+  		 params.require(:user).permit(:email, :password, :firstname, :lastname, :address, :phonenumber)
+  	end
 end
